8b29cb5ba5d26d5f216e7a6b0f02ebb3f4bfa1c0
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-10 21:10:42 +0530

    fixed bug in feedback routes

--- Backend/Routes/Contact.js
+++ Backend/Routes/Contact.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
 import { sendFeedback } from "../controllers/Feedback.js"
-const router=Router();
-router.post("/sendFeedback", sendFeedback);
-export default router;
+const contactRoutes=Router();
+contactRoutes.post("/sendFeedback", sendFeedback);
+export default contactRoutes;
