e2e2f38e910ce3380362fae4995655e3c14078a5
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-06-10 21:12:43 +0530

    fixed profile routes

--- Backend/Routes/Profile.js
+++ Backend/Routes/Profile.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getUserDetails } from "../controllers/Profile.js";
 import { verifyJWT } from "../middlewares/auth.js";
-const router=Router();
-router.get("/getUserDetails",verifyJWT,getUserDetails)
-export default router;
+const profileRoutes=Router();
+profileRoutes .get("/getUserDetails",verifyJWT,getUserDetails)
+export default profileRoutes ;
