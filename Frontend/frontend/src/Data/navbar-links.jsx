88afaeba1dec2c05e843ec5525ce2e4d510b73c3
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-07-01 20:32:24 +0530

    created the navbar links for diffrent pages.jsx

---
+++ Frontend/frontend/src/Data/navbar-links.jsx
@@ -0,0 +1,18 @@
+export const NavBarLinks =[
+    {
+        title:"Home",
+        path : "/",
+    },
+    {
+        title:"Courses",
+        path:"/courses",
+    },
+    {
+        title:"Upload",
+        path:"/upload",
+    },
+    {
+        title:"Contact Us",
+        path:"/contact",
+    },
+]
