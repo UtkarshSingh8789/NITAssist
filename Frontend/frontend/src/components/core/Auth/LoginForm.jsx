aaca4eada0d560302d378c546bbb29f8e5287476
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-07-03 20:23:36 +0530

    comleted the component of login form

--- Frontend/frontend/src/components/core/Auth/LoginForm.jsx
+++ Frontend/frontend/src/components/core/Auth/LoginForm.jsx
@@ -18,8 +18,65 @@ const LoginForm=()=>{
             [e.target.name]:e.target.value,
         }))
     }
-  const handleOnSubmit=(e)=>{
-    e.preventDefault();
-    dispatch(login(email,password,navigate));// this is used to dispat
-  }
+    const handleOnSubmit=(e)=>{
+        e.preventDefault();
+        dispatch(login(email,password,navigate));// this is used to dispat
+    }
+    return (
+    <div>
+      <form   onSubmit={handleOnSubmit}
+      className="mt-6 flex w-full flex-col gap-y-4">
+      <label className="w-full">
+        <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-900">
+          Email Address <sup className="text-pink-200">*</sup>
+        </p>
+        <input
+          required
+          type="text"
+          name="email"
+          value={email}
+          onChange={handleOnChange}
+          placeholder="Enter email address"
+          className="form-style w-full"
+        />
+      </label>
+      <label className="relative">
+        <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-900">
+          Password <sup className="text-pink-200">*</sup>
+        </p>
+        <input
+          required
+          type={showPassword ? "text" : "password"}
+          name="password"
+          value={password}
+          onChange={handleOnChange}
+          placeholder="Enter Password"
+          className="form-style w-full !pr-10"
+        />
+        <span
+          onClick={() => setShowPassword((prev) => !prev)}
+          className="absolute right-3 top-[38px] z-[10] cursor-pointer"
+        >
+          {showPassword ? (
+            <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF" />
+          ) : (
+            <AiOutlineEye fontSize={24} fill="#AFB2BF" />
+          )}
+        </span>
+        <Link to="/forgot-password">
+          <p className="mt-1 ml-auto max-w-max text-xs text-blue-900">
+            Forgot Password
+          </p>
+        </Link>
+      </label>
+      <button
+        type="submit"
+        className="mt-6 rounded-[8px] bg-green-500 py-[8px] px-[12px] font-medium text-richblack-900"
+      >
+        Sign In
+      </button>
+      </form>
+    </div>
+  )
 }
+export default LoginForm
