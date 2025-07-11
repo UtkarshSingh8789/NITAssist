b9eb61aa0289ae0e04de5d2285d8e21b8cd63c5d
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-07-11 17:34:16 +0530

    completed the frontend part of the project

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import './App.css';
+import Footer from './components/common/Footer.jsx';
+import Navbar from './components/common/Navigation.jsx';
+import Home from './pages/Home.jsx';
+import { Route, Routes, useNavigate} from 'react-router-dom';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import Desktop from './pages/Desktop.jsx';
+import Courses from './pages/Courses.jsx';
+import Course from './components/core/Courses/Course.jsx';
+import Upload from './pages/Upload.jsx';
+import Contact from './pages/Contact.jsx';
+import { useDispatch } from 'react-redux';
+import OpenRoute from './components/core/Auth/OpenRoute.jsx';
+import PrivateRoute from './components/core/Auth/PrivateRoute.jsx';
+import Error from './pages/Error.jsx';
+import { useEffect } from 'react';
+import { getUserDetails } from './services/operations/apiforprofile.jsx';
+import SubjectNotes from "../src/components/core/Courses/SubjectNotes.jsx";
+import DirectUploads from './components/core/fileUpload/DirectUploads.jsx';
+import DriveLinkUploads from './components/core/fileUpload/DriveLinkUploads.jsx';
+import MyProfile from './components/core/Dashboard/MyProfile.jsx';
+export default function App() {
+  // const { user } = useSelector((state) => state.profile);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      const token = JSON.parse(localStorage.getItem("token"))
+      console.log("print token in app.js ",token);
+      dispatch(getUserDetails(token, navigate))
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={
+          <OpenRoute>
+            <Login />
+          </OpenRoute>
+        }> </Route>
+        <Route path='/signup' element={
+          <OpenRoute>
+          <Signup />
+        </OpenRoute>
+        }></Route>
+        <Route path='/dashboard' element={
+          <PrivateRoute>
+            <Desktop />
+          </PrivateRoute>
+        }>
+        </Route>
+        <Route path="/dashboard/my-profile" element={
+          <PrivateRoute>
+            <MyProfile />
+          </PrivateRoute>
+        } />
+        <Route path='/courses' element={<Courses />}></Route>
+        <Route path='/courses/:Department' element={
+          <PrivateRoute>
+            <Course />
+          </PrivateRoute>
+        }></Route>
+        <Route path='/courses/:Department/subjectNotes/:subjectName' element={
+          <PrivateRoute>
+            <SubjectNotes/>
+          </PrivateRoute>
+        }></Route>
+        <Route path='/upload' element={
+          <PrivateRoute>
+            <Upload />
+          </PrivateRoute>
+        }></Route>
+        <Route path='/upload/uploadLocalfile' element={
+          <PrivateRoute>
+            <DirectUploads />
+          </PrivateRoute>
+        }></Route>
+        <Route path='/upload/uploadUsingDriveLink' element={
+          <PrivateRoute>
+            <DriveLinkUploads/>
+          </PrivateRoute>
+        }></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
-
-export default App
--- Frontend/frontend/src/main.jsx
+++ Frontend/frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducer/index.jsx';
+const store = configureStore(
+  {
+    reducer:rootReducer,
+  }
 )
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <Toaster />
+    </Provider>
+);
+
