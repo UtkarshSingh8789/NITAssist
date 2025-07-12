101925eff6ba8983c405bbc9ec964f26896c2d77
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-07-12 09:05:36 +0530

    fixed bug in apis handling

--- Frontend/frontend/src/services/apis.jsx
+++ Frontend/frontend/src/services/apis.jsx
@@ -1,4 +1,4 @@
-const BASE_URL=`http://localhost:5001/api/v1/`
+const BASE_URL=`http://localhost:5001/api/v1`
 export const endpoints={
     LOGIN_API:BASE_URL+`/auth/login`,
     SIGNUP_API:BASE_URL+`/auth/signup`
