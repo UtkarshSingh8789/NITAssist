6858e9d9e88ff8d9a1059c157a86901ec138830e
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-07-12 09:04:28 +0530

    fixed the bug in footer compnent

--- Frontend/frontend/src/components/common/Footer.jsx
+++ Frontend/frontend/src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className='w-full flex py-10  bg-[#9BA4B5] '>
             <div className="font-semibold mx-auto">
-                <p>&copy; {new Date().getFullYear()} ColleGmate. All rights reserved.</p>
+                <p>&copy; {new Date().getFullYear()} NITAssist. All rights reserved.</p>
             </div>
             <div>
 
